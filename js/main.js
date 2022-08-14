b92ee6b2271a81c75d1423f3e4499b37c13c356c
Silvana Bendahan Gloria <sbendahan81@gmail.com>
2022-08-14 17:03:16 -0300

    JS

--- js/main.js
+++ js/main.js
@@ -1,55 +1,70 @@
-let tramos = prompt ("¿Es un viaje de IDA? (ingrese 1) ¿o es un viaje de IDA y VUELTA? (ingrese 2)");
-
 let cant_pasajeros = parseInt (prompt( "¿Cuántos pasajeros van a viajar?"));
+
+let edad_pasajero;
 
 for (i=0; i=cant_pasajeros; i++) {
     let edad_pasajero = parseInt(prompt("Ingrese la edad de cada pasajero que viaja"));
+    return edad_pasajero;
 }
+
+let tramos = prompt ("¿Es un viaje de IDA? (ingrese 1) ¿o es un viaje de IDA y VUELTA? (ingrese 2)");
 
 let pago = prompt ("¿Cómo va a pagar? En efectivo: ingrese 1; con código QR: ingrese 2 o con tarjeta de crédito: ingrese: 3");
 
 let consumidor = prompt ("¿Es consumidor final?");
 
-let tarifa_impuestos = 200;
-let tarifa_base = 800;
-let tarifa_tramos;
-
-if (tramos = 1) {
-    tarifa_tramos = tarifa_base * 0.8;
-} else {
-    tarifa_tramos = tarifa_base;
-}
+let tarifa_base = 1000;
+let tarifa_impuestos = tarifa_base * 0.65;
 
 let tarifa_edad;
 
 if(edad_pasajero<2) {
-    tarifa_impuestos + tarifa_tramos * 0;
+    tarifa_edad = tarifa_impuestos + tarifa_tramos * 0;
 } else if (edad_pasajero >=2 && edad_pasajero <12) {
-    tarifa_impuestos + tarifa_tramos * 0.5;
+    tarifa_edad = tarifa_impuestos + tarifa_tramos * 0.5;
 } else if (edad_pasajero >= 65) {
-    tarifa_impuestos + tarifa_tramos * 0.8;
+    tarifa_edad = tarifa_impuestos + tarifa_tramos * 0.8;
 } else {
-    tarifa_impuestos + tarifa_tramos;
+    tarifa_edad = tarifa_impuestos + tarifa_tramos;
 }
 
+let tarifa_grupal; //sumar a todos los pasajeros
+
+let tarifa_tramos;
+
+if (tramos = 1) {
+    tarifa_tramos = tarifa_grupal * 0.75;
+} else {
+    tarifa_tramos = tarifa_grupal;
+}
+
+let tarifa_pago;
+
 if (pago = 1) {
-    tarifa_edad * 1;
+    tariga_pago = tarifa_tramos * 1;
 } else if (pago = 2) {
-    tarifa_edad * 1.10;
+    tarifa_pago = tarifa_tramos * 1.10;
 } else {
     let cuotas = parseInt(prompt ("¿En cuántas cuotas va a pagar? Ingrese 3, 6 o 12 cuotas"));
     if (cuotas = 3) {
-        tarifa_edad * 1.30;
+        tarifa_pago = tarifa_tramos * 1.30;
     } else if (cuotas = 6) {
-        tarifa_edad * 1.6;
+        tarifa_pago = tarifa_tramos * 1.6;
     } else { 
-        tarifa_edad * 2.2;
+        tarifa_pago = tarifa_tramos * 2.2;
     }
 }
 
+let tarifa_consumidor;
+
 if (consumidor = 1) {
-    
+    tarifa_consumidor = tarifa_pago * 1.21;
+    alert ("El total a pagar para ," + cant_pasajeros + "pasajeros en viaje es de $AR: ", + tarifa_consumidor);
+
+} else {
+    tarifa_consumidor = tarifa_pago;
+    alert ("El total a pagar para ," + cant_pasajeros + "pasajeros en viaje es de $AR: ", + tarifa_consumidor + "sin IVA");
 }
 
-alert ("El total a pagar para ," + cant_pasajeros + "pasajeros en viaje de ", );
+
 

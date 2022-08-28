f25c6a62dde6c20b5b4a075e523ae70b8f067956
Silvana Bendahan Gloria <sbendahan81@gmail.com>
2022-08-28 20:20:13 -0300

    Primera preentrega

--- js/main.js
+++ js/main.js
@@ -30,6 +30,10 @@ class Destino {
         return this.tarifa;
     }
 
+    impuestos () {
+        return this.tarifa *1.65;
+    }
+
 };
     
 
@@ -39,29 +43,23 @@ let lugar3 = new Destino ("TOKIO", 2200, 18362);
 let lugar4 = new Destino ("ESTAMBUL", 1800, 12237);
 
 
-/*
-let impuesto_gobierno = Destino.tarifa * 0.65;
-
-let impuestos = Destino.tarifa + impuesto_gobierno;
-*/
-
 
 function tarifa_impuestos (lugar){
     if (lugar == lugar1.nombre) {
-        return lugar1.tarifa_base () * 1.65;
+        return lugar1.impuestos ();
 
     } else if (lugar == lugar2.nombre) {
-       return lugar2.tarifa_base () * 1.65;
+       return lugar2.impuestos () ;
 
     } else if (lugar  == lugar3.nombre) {
-       return lugar3.tarifa_base () * 1.65;
+       return lugar3.impuestos () ;
 
     } else if (lugar  == lugar4.nombre) {
-       return lugar4.tarifa_base () * 1.65;
+       return lugar4.impuestos () ;
 
     } else {
         alert ("No volamos a ese destino");
-        //aca no puedo poner un break???
+        
     }
         
 }
@@ -70,20 +68,25 @@ tarifa_impuestos (ingreso);
 
 let tarifa_con_impuestos = tarifa_impuestos (ingreso);
 
-console.log (lugar2.tarifa_base()); //ok
+
+
 console.log (tarifa_impuestos (ingreso));  //ok
 console.log (tarifa_con_impuestos); //ok
 
 
 //Cantidad de pasajeros
+
 cant_pasajeros = parseInt (prompt ("¿Cuántos pasajeros van a viajar?"));
 
+
 let lista_pasajeros = []; 
+
 
 class Pasajeros {
     constructor (edad) {
         this.edad = edad
     }
+    
 };
 
 
@@ -94,58 +97,61 @@ for (let i=0; i < cant_pasajeros; i++) {
     let nuevo_pasajero = new Pasajeros (edad);
 
     lista_pasajeros.push (nuevo_pasajero);
-    
+
 }
 
 console.log (lista_pasajeros); //ok
 
 
 //Tarifa de todos los pasajeros: tarifa_edad y tarifa_grupal
+
 let INF = tarifa_con_impuestos * 0.1 ;
 let CHD = tarifa_con_impuestos * 0.5 ;
 let SNR = tarifa_con_impuestos * 0.8 ;
 let ADT = tarifa_con_impuestos ;
 
-function tarifa_edad (edad){
-    if(edad < 2) {
-        return {
-            INF
-        };
-       
-    } else if (edad >= 2 && edad < 12) {
-        return {
-            CHD
-        };
 
-    } else if (edad >= 65) {
-        return {
-            SNR
-        };
+function tarifa_edad (elemento){
+    if(elemento < 2) {
+       return {
+        INF
+        }
+    
+    } else if ((elemento >= 2) && (elemento < 12)) {
+       return {
+        CHD
+        }
+
+    } else if (elemento >= 65) {
+       return {
+        SNR
+        }
 
     } else {
-        return {
-            ADT
+       return {
+        ADT
         }
     }
 }
 
+tarifa_edad (edad);
 
-let tarifa_grupal = lista_pasajeros.map(tarifa_edad); //toma siempre el else de "ADT", coregir!!
-console.log (tarifa_grupal); //ok pero no :)
+let tarifa_grupal = lista_pasajeros.map(tarifa_edad); //toma siempre el else de "ADT".....
+console.log (tarifa_grupal); 
 
 
 
 //Sumatoria del array tarifa_grupal : suma
 
 function sumar_tarifa_grupal (acu, precio) {
-    acu = acu + precio.ADT ; //ojo no va ADT, corregir!!!!
+    acu = acu + precio.ADT ; //ojo no va ADT, corregir, pero no se como....
     return acu
 };
 
 let suma = tarifa_grupal.reduce (sumar_tarifa_grupal, 0) ;
 console.log (suma);
 
-console.log (suma); //esta mal, pero no tan mal
+console.log (suma); 
 
 
 //Ida o I/V: tarifa_tramos

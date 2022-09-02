dd038006e966a0ffe56c85242cb8a1ceb7e61726
Silvana Bendahan Gloria <sbendahan81@gmail.com>
2022-09-02 18:21:17 -0300

    Iniciando DOM

--- js/main.js
+++ js/main.js
@@ -26,14 +26,9 @@ class Destino {
         this.distancia = distancia;
     }
 
-    tarifa_base () {
-        return this.tarifa;
-    }
-
     impuestos () {
-        return this.tarifa *1.65;
+        return this.tarifa * 1.65;
     }
-
 };
     
 
@@ -43,25 +38,12 @@ let lugar3 = new Destino ("TOKIO", 2200, 18362);
 let lugar4 = new Destino ("ESTAMBUL", 1800, 12237);
 
 
-
 function tarifa_impuestos (lugar){
-    if (lugar == lugar1.nombre) {
-        return lugar1.impuestos ();
 
-    } else if (lugar == lugar2.nombre) {
-       return lugar2.impuestos () ;
+    const destinos = [lugar1, lugar2, lugar3, lugar4]; //lista de destinos
+    const encontrado = destinos.find (Destino => Destino.nombre == lugar)
 
-    } else if (lugar  == lugar3.nombre) {
-       return lugar3.impuestos () ;
-
-    } else if (lugar  == lugar4.nombre) {
-       return lugar4.impuestos () ;
-
-    } else {
-        alert ("No volamos a ese destino");
-        
-    }
-        
+    return encontrado.impuestos ();
 }
 
 tarifa_impuestos (ingreso);
@@ -76,11 +58,7 @@ console.log (tarifa_con_impuestos); //ok
 
 cant_pasajeros = parseInt (prompt ("¿Cuántos pasajeros van a viajar?"));
 
-
-
 let lista_pasajeros = []; 
-
-
 
 let INF = tarifa_con_impuestos * 0.1 ;
 let CHD = tarifa_con_impuestos * 0.5 ;
@@ -93,14 +71,27 @@ class Pasajeros {
     constructor (edad) {
         this.edad = edad
     }  
+
+   /* set edad (edad) {
+        this.edad = edad
+    };
+
+    get edad () {
+        return this.edad;
+    };*/
+
 };
 
 
 for (let i=0; i < cant_pasajeros; i++) {
     
-    edad = parseInt(prompt("Ingrese la edad de/del los pasajero/s"));
+    let edad = parseInt(prompt("Ingrese la edad de/del los pasajero/s"));
     
     let nuevo_pasajero = new Pasajeros (edad);
+
+   /* nuevo_pasajero.edad = x;
+
+    nuevo_pasajero.edad*/
 
     lista_pasajeros.push (nuevo_pasajero);
 
@@ -111,65 +102,36 @@ console.log (lista_pasajeros); //ok
 
 //Tarifa de todos los pasajeros: tarifa_edad y tarifa_grupal
 
-function tarifa_edad (elemento){
-    if(elemento < 2) {
-       return {
-        INF
-        }
+function tarifa_edad (edad){
+    if(edad < 2) {
+       return INF
     
-    } else if ((elemento >= 2) && (elemento < 12)) {
-       return {
-        CHD
-        }
-    } else if (elemento >= 65) {
-       return {
-        SNR
-        }
+    } else if ((edad >= 2) && (edad < 12)) {
+       return CHD
+
+    } else if (edad >= 65) {
+       return SNR
+
     } else {
-       return {
-        ADT
-        }
+       return ADT
     }
 }
 
 
-let tarifa_grupal = lista_pasajeros.map(tarifa_edad); //toma siempre el else de "ADT".....
+const tarifa_grupal = lista_pasajeros.map((x) => tarifa_edad(x.edad)); 
 console.log (tarifa_grupal); 
 
-/*
-function tarifa_edad2 () {
-    for (let tarifa_edad of lista_pasajeros) {
-        if(tarifa_edad < 2) {
-            return INF
-            
-        } else if ((tarifa_edad >= 2) && (tarifa_edad < 12)) {
-        return CHD
 
-        } else if (tarifa_edad >= 65) {
-        return SNR
-
-        } else {
-        return ADT
-        }
-    }
-}
-
-let sumar_tarifa = lista_pasajeros.map (tarifa_edad2, lista_pasajeros);
-console.log (sumar_tarifa);
-
-//let tarifa_grupal = lista_pasajeros.forEach(tarifa_edad2(lista_pasajeros)); 
-console.log (tarifa_grupal); 
-
-*/
 
 //Sumatoria del array tarifa_grupal : suma
 
 function sumar_tarifa_grupal (acu, precio) {
-    acu = acu + precio.ADT ; //ojo no va ADT, corregir, pero no se como....
+    acu = acu + precio ; 
     return acu
 };
 
-let suma = tarifa_grupal.reduce (sumar_tarifa_grupal, 0) ;
+
+const suma = tarifa_grupal.reduce((a,b,c,d) => sumar_tarifa_grupal (a,b), 0 ) ;
 console.log (suma);
 
 
@@ -186,31 +148,42 @@ if (tramos == 1) {
 //Forma de pago: tarifa_pago
 pago = prompt ("¿Cómo va a pagar? En efectivo: Ingrese 1; con código QR: Ingrese 2 o con tarjeta de crédito: Ingrese: 3");
 
-if (pago == 1) {
-    tarifa_pago = tarifa_tramos;
-} else if (pago == 2) {
-    tarifa_pago = tarifa_tramos * 1.10;
-} else {
-    let cuotas = parseInt(prompt ("¿En cuántas cuotas va a pagar? Ingrese 3, 6 o 12"));
-    if (cuotas == 3) {
-        tarifa_pago = tarifa_tramos * 1.30;
-    } else if (cuotas == 6) {
-        tarifa_pago = tarifa_tramos * 1.6;
-    } else { 
-        tarifa_pago = tarifa_tramos * 2.2;
+function ajustar_tarifa_pago () {
+    if (pago == 1) {
+        return tarifa_tramos;
+
+    } else if (pago == 2) {
+        return tarifa_tramos * 1.10;
+
+    } else {
+        return tarjeta (); 
     }
 }
+
+function tarjeta () {
+    let cuotas = parseInt(prompt ("¿En cuántas cuotas va a pagar? Ingrese 3, 6 o 12"));
+
+        if (cuotas == 3) {
+            return tarifa_tramos * 1.30;
+        } else if (cuotas == 6) {
+            return tarifa_tramos * 1.6;
+        } else { 
+            return tarifa_tramos * 2.2;
+        }
+}
+
+tarifa_pago = ajustar_tarifa_pago ();
 
 //Con o sin IVA (21%): tarifa_consumidor
 consumidor =  prompt ("Si es consumidor final ingrese: 1, sino ingrese: 2");
 
 if (consumidor == 1) {
     tarifa_consumidor = Math.round (tarifa_pago * 1.21);
-   alert ("El total a pagar para " + cant_pasajeros + " pasajero/s es de $AR: " + tarifa_consumidor );
+   alert ("El total a pagar para " + cant_pasajeros + " pasajero/s es de USD: " + tarifa_consumidor );
 
 } else {
     tarifa_consumidor = Math.round (tarifa_pago);
-    alert ("El total a pagar para " + cant_pasajeros + " pasajero/s es de $AR: " + tarifa_consumidor + " sin IVA.-");
+    alert ("El total a pagar para " + cant_pasajeros + " pasajero/s es de USD: " + tarifa_consumidor + " sin IVA.-");
 }
 
 

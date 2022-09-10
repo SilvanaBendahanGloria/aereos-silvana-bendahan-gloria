9ea0db36289966c4b02dfc728220d031a973e2e3
Silvana Bendahan Gloria <sbendahan81@gmail.com>
2022-09-10 14:54:57 -0300

    Boton pago

--- js/main2.js
+++ js/main2.js
@@ -5,12 +5,7 @@ let impuesto_dolares = 1.65;
 let lista_pasajeros = [];
 let nombre_pasajero = document.getElementById("nombre_pasajero"); 
 
-let agregar_texto_ida = document.getElementById("texto_ida");
-let agregar_opciones_ida = document.getElementById("opciones_ida");
 
-
-let agregar_texto_pago = document.getElementById("texto_pago");
-let agregar_opciones_pago = document.getElementById ("opciones_pago");
 let agregar_en_cuotas = document.getElementById("en_cuotas");
 let agregar_cantidad_cuotas = document.getElementById("cantidad_cuotas");
 let agregar_check_uno = document.getElementById("check_uno");
@@ -20,7 +15,7 @@ let boton_primer_paso = document.getElementById("primer_paso");
 let agregar_boton_edades = document.getElementById("boton_edades");
 let agregar_boton_forma_pago = document.getElementById("boton_forma_pago");
 
-//Clases
+//CIUDADES
 class Destino {
     constructor (nombre, tarifa, distancia) {
         this.nombre = nombre;
@@ -57,23 +52,16 @@ function tomar_lugar () {
     }
 };
 
-
-let tu_viaje = document.getElementById('tipo_de_viaje');
-let seleccion_viaje;
+//IDA Y VUELTA
+let tu_viaje;
 
 function tipo_de_viaje () {
-    
-    tu_viaje.addEventListener("change", (e) => {
-        e.preventDefault();
-        seleccion_viaje = e.target.value;
-        
-    });
-    //return seleccion_viaje;
-    console.log(seleccion_viaje);
+    tu_viaje = document.getElementById("tipo_de_viaje").value;
+    console.log (tu_viaje);
 }
 
 
-
+//EDADES
 let num_pasajeros_2;
 let num_pasajeros_11;
 let num_pasajeros_64;
@@ -91,23 +79,73 @@ function tomar_edades(){
 }
 
 
+//FORMA DE PAGO
+
+let tu_pago;
+let texto_cuotas;
+let cantidad_cuotas;
+
+function forma_de_pago () {
+    tu_pago = document.getElementById("forma_de_pago").value;
+    console.log (tu_pago);
+
+    if (tu_pago === "TARJETA") {
+        texto_cuotas = document.getElementById("texto_cuotas");
+        let titulo_cuotas;
+        titulo_cuotas += '<h3> ¿En cuantas cuotas vas a pagar? </h3>';
+        texto_cuotas.innerHTML = titulo_cuotas;
+/*
+        cantidad_cuotas = document.getElementById("cantidad_cuotas");
+        
+        let agregar_cuotas = () => {
+            let num_cuotas = document.createElement('select');
+            num_cuotas.id = "cantidad_cuotas";
+            
+            let tres_cuotas = document.createElement('option');
+            tres_cuotas.value = "1";
+            tres_cuotas.text = "3 cuotas";
+            let seis_cuotas = document.createElement('option');
+            seis_cuotas.value = "2";
+            seis_cuotas.text = "6 cuotas";
+            let doce_cuotas = document.createElement('option');
+            doce_cuotas.value = "3";
+            doce_cuotas.text = "12 cuotas";
+            
+            num_cuotas.appendChild(tres_cuotas);
+            num_cuotas.appendChild(seis_cuotas);
+            num_cuotas.appendChild(doce_cuotas);
+            
+            let cantidad_cuotas.appendChild("agregar_cuotas"); 
+        } */
+    };    
+}
+
+let tipo_consumidor;
+let texto_consumidor;
+
+function tipo_de_consumidor () {
+    tipo_consumidor = document.getElementById("texto_consumidor");
+    texto_consumidor += '<h2> 5) ¿Que tipo de factura necesitas? </h2>';
+    tipo_consumidor.innerHTML = texto_consumidor;
+}
+
+
+let boton_de_pago;
+let texto_boton_pago;
+
+function boton_pago () {
+    boton_de_pago = document.getElementById("boton_pago");
+    texto_boton_pago += '<button type="button" class="btn btn-dark center">Calculá tu viaje</button>';
+    boton_de_pago.innerHTML = texto_boton_pago; 
+}
+
 
 boton_primer_paso.addEventListener("click", tomar_lugar);
-boton_primer_paso.addEventListener("click", tomar_edades);
 boton_primer_paso.addEventListener("click", tipo_de_viaje);
+boton_primer_paso.addEventListener("click", tomar_edades);
+boton_primer_paso.addEventListener("click", forma_de_pago);
+boton_primer_paso.addEventListener("click", tipo_de_consumidor);
+boton_primer_paso.addEventListener("click", boton_pago);
 
 
 
-/*
-pasajero_inf = num_pasajeros_2 * tomar_lugar() * 0.1;
-pasajero_chd = num_pasajeros_11 * tomar_lugar() * 0.5;
-pasajero_adt = num_pasajeros_64 * tomar_lugar();
-pasajero_snr = num_pasajeros_65 * tomar_lugar() * 0.8;
-
-let sumar;
-
-function suma () {
-   sumar = pasajero_inf + pasajero_chd + pasajero_adt + pasajero_snr;
-   return sumar;
-}
-*/
